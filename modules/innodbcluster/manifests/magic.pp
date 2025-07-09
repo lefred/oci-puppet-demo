29aaa4288348ac4c3283633dbc27b0d6f707d538
Frederic Descamps <lefred.descamps@gmail.com>
2025-07-09 14:02:21 +0200

    replace debug by warning
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/innodbcluster/manifests/magic.pp
+++ modules/innodbcluster/manifests/magic.pp
@@ -10,9 +10,8 @@ class innodbcluster::magic {
 
   $members = lookup('innodbcluster::members', Optional[Array[String]], 'first', undef)
   $cluster_node = innodbcluster::seed_node($members, $user)
-  notice("Cluster members are ${members}")
   warning("Cluster members are ${members}")
-  debug("Cluster node is ${cluster_node}")
+  warning("Cluster node is ${cluster_node}")
   $this_host = $facts['networking']['fqdn']
 
   if defined(Class['innodbcluster::clusterset']) {
