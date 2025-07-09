a5b4c8212e9cb7fb2731eb94257b710cf16f4e22
Frederic Descamps <lefred.descamps@gmail.com>
2025-07-09 20:25:04 +0200

    fix eol
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/innodbcluster/manifests/magic.pp
+++ modules/innodbcluster/manifests/magic.pp
@@ -41,7 +41,7 @@ class innodbcluster::magic {
          command => "mysqlsh --login-path=root@localhost --no-wizard root@localhost -- dba configure-instance --clusterAdmin=${user} --clusterAdminPassword='${password}' --restart=true && /bin/sleep 5",
          path    => ['/usr/bin', '/bin'],
          environment => ['MYSQL_TEST_LOGIN_FILE=/root/.mylogin.cnf'],
-         unless  => "mysql --login-path=root@localhost -BN -e 'select user from mysql.user where user = \"${user}\" and host = \"%\"' | grep $user > /dev/null",
+         unless  => "mysql --login-path=root@localhost -BN -e 'select user from mysql.user where user = \"${user}\" and host = \"%\"' | grep $user > /dev/null";
   "change_instance_label":
             command => "mysqlsh ${user}@${this_host} --no-wizard -- cluster set-instance-option ${host_with_port} label ${this_host}",
             path    => ['/usr/bin', '/bin'],
