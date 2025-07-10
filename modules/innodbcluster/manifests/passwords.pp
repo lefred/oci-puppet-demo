b36bec2e15fe435201f180bec31969e4aeb5bf96
Frederic Descamps <lefred.descamps@gmail.com>
2025-07-10 23:23:54 +0200

    change unless path
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/innodbcluster/manifests/passwords.pp
+++ modules/innodbcluster/manifests/passwords.pp
@@ -6,7 +6,7 @@ class innodbcluster::passwords ( $user_dir = "/root/"){
 
   exec {'set_admin_pwd_file':
         command   => "expect -d -c 'set timeout -1; spawn mysql_config_editor set --login-path=client --skip-warn --user=${user} --password; expect \"Enter password:\"; send \"${password}\n\"; expect eof'",
-        unless    => "mysql_config_editor print --all | grep -q 'user = \"${user}\"'",
+        unless    => "/usr/libexec/mysqlsh/mysql_config_editor print --all | grep -q 'user = \"${user}\"'",
         environment => ["MYSQL_TEST_LOGIN_FILE=${user_dir}.mylogin.cnf", "HOME=${user_dir}"],
         path      => ['/usr/bin', '/usr/libexec/mysqlsh/'],
  }
