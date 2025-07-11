78c5842f2918e00e9d351ed5eca6013f4003fa65
Frederic Descamps <lefred.descamps@gmail.com>
2025-07-11 13:47:07 +0200

    fix ownership
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- modules/innodbcluster/manifests/passwords.pp
+++ modules/innodbcluster/manifests/passwords.pp
@@ -4,11 +4,18 @@ class innodbcluster::passwords ( $user_dir = "/root/"){
  $user = $innodbcluster::adminuser
  $password = $innodbcluster::adminpassword
 
-  exec {'set_admin_pwd_file':
+  exec {
+      'set_admin_pwd_file':
         command   => "expect -d -c 'set timeout -1; spawn mysql_config_editor set --login-path=client --skip-warn --user=${user} --password; expect \"Enter password:\"; send \"${password}\n\"; expect eof'",
         unless    => "/usr/libexec/mysqlsh/mysql_config_editor print --all | grep -q 'user = \"${user}\"'",
         environment => ["MYSQL_TEST_LOGIN_FILE=${user_dir}.mylogin.cnf", "HOME=${user_dir}"],
         path      => ['/usr/bin', '/usr/libexec/mysqlsh/'],
+        notify    => Exec['set_admin_pwd_file_ownership'];
+      'set_admin_pwd_file_ownership':
+        command   => "chown ${user}:${user} ${user_dir}.mylogin.cnf",
+        path      => ['/usr/bin', '/usr/libexec/mysqlsh/'],
+        require   => Exec['set_admin_pwd_file'],
+        refreshonly => true;
  }
 
 }
